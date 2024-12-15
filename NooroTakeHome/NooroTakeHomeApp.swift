2c47a3ed38fadfe4e78ace48f9abcf998b08999d
Aaron Adelman <Aaron.Solomon.Adelman@gmail.com>
2024-12-15 15:06:55 +0200

    Initial Commit

---
+++ NooroTakeHome/NooroTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NooroTakeHomeApp.swift
+//  NooroTakeHome
+//
+//  Created by אהרן שלמה אדלמן on 15/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct NooroTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
